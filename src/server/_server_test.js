04f5040b9d22cc78fd3ff031429c4326b4878c87
Callum Leggat <calleggat@gmail.com>
2014-08-06 13:00:35 +0100

    Test that server returns hello world

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -10,10 +10,24 @@ exports.tearDown = function(done) {
 	});
 };
 
-exports.testServerResponseToGetRequests = function(test) {
-    server.start();
-    http.get("http://localhost:8080", function(response) {
-	    response.on("data", function(){});
-	    test.done();
-    });
+exports.test_serverReturnsHelloWorld = function(test) {
+	server.start(8080);
+
+	var request = http.get( "http://localhost:8080" );
+	request.on( "response", function(response) {
+		var receivedData = false;
+		response.setEncoding("utf8");
+
+		test.equals(200, response.statusCode, "status code");
+
+		response.on("data", function(chunk){
+			receivedData = true;
+			test.equals("Hello World", chunk, "response text");
+		});
+
+		response.on("end", function() {
+			test.ok( receivedData, "Should have received response data");
+			test.done();
+		});
+	});
 };
